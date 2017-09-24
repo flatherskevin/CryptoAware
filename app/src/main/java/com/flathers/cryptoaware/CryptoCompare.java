0a9c813572875a1449c42cc1b38137d3eeec11c8
flatherskevin <flatherskevin@gmail.com>
2017-09-24 00:51:43 -0400

    Added toast capabilities

--- app/src/main/java/com/flathers/cryptoaware/CryptoCompare.java
+++ app/src/main/java/com/flathers/cryptoaware/CryptoCompare.java
@@ -1,11 +1,17 @@
 package com.flathers.cryptoaware;
-
+import android.widget.Toast;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
 /**
  * Created by flath on 9/17/2017.
  */
 
-public class CryptoCompare {
+public class CryptoCompare{
 
     public void CryptoCompare(){
+    }
+
+    public void toast(Context context, String toastDisplay){
+        Toast.makeText(context, toastDisplay, Toast.LENGTH_LONG).show();
     }
 }

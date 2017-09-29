b61dc4737f52866be5263177c5ee7e140feef1e5
flatherskevin <flatherskevin@gmail.com>
2017-09-28 23:28:28 -0400

    Coin selection is now fully functional. Proper database storage takes place, and recurring updates are functional. Application is ready for alpha deployment on PlayStore

--- app/src/main/java/com/flathers/cryptoaware/WatchingActivity.java
+++ app/src/main/java/com/flathers/cryptoaware/WatchingActivity.java
@@ -13,6 +13,7 @@ import android.view.ViewGroup;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -26,10 +27,10 @@ import android.widget.TableRow;
 import android.widget.TextView;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import java.lang.reflect.Array;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 
 public class WatchingActivity extends AppCompatActivity {
@@ -38,8 +39,8 @@ public class WatchingActivity extends AppCompatActivity {
     private static final String STATE_TAG = "StateChange";
     private static final String COIN_VIEW = "CoinView";
     private static final String BUTTON_CLICK = "ButtonClick";
-    private String[] demoMarkets = new String[] {"Yobit", "Kraken"};
-    private String[] demoCoins = new String[] {"LTC","DASH"};
+    private String[] userMarkets = new String[] {"Kraken"};
+    private ArrayList<String> userCoins = new ArrayList<String>();
     private CoinList coinList;
     private WatchingCoinsDb watchingCoinsDb;
 
@@ -53,12 +54,11 @@ public class WatchingActivity extends AppCompatActivity {
         //Make sure database is setup on install
         watchingCoinsDb = new WatchingCoinsDb(mContext);
 
-        //TODO: this loading onCreate is a rough and dirty way to make it work. Fix the async issue
+        //Prepare initial coinList
         coinList = new CoinList(mContext);
 
-        //TODO: remove dummy coins once coinPicker is fully good to go
-        PriceMultiFull priceMultiFull = new PriceMultiFull(mContext, demoCoins, "Yobit");
-        priceMultiFull.sendRequest();
+        //Render current coins
+        renderCoinListView();
 
         ImageButton addCoinButton = (ImageButton) findViewById(R.id.watching_imgbtn_addCoin);
 
@@ -69,21 +69,41 @@ public class WatchingActivity extends AppCompatActivity {
                     //
                     ArrayList<String> newArrayList = coinList.getCoinArrayList();
 
-                    String[] coinsAvailable = newArrayList.toArray(new String[newArrayList.size()]);
+                    final String[] coinsAvailable = newArrayList.toArray(new String[newArrayList.size()]);
 
                     //Create a dialog to choose coins from
                     final Dialog dialog = new Dialog(mContext);
                     dialog.setContentView(R.layout.coin_selection);
                     dialog.setTitle("Add Coin");
+                    Button btnSelect = (Button) dialog.findViewById(R.id.watching_btn_confirm);
+                    Button btnCancel = (Button) dialog.findViewById(R.id.watching_btn_cancel);
 
                     //Usage of a NumberPicker is satisfactory for now to choose coins
                     //TODO: add a search bar since there are so many coins
-                    NumberPicker selector = (NumberPicker) dialog.findViewById(R.id.watching_numpkr_selector);
+                    final NumberPicker selector = (NumberPicker) dialog.findViewById(R.id.watching_numpkr_selector);
                     selector.setMinValue(0);
                     selector.setMaxValue(coinsAvailable.length - 1);
                     Arrays.sort(coinsAvailable);
                     selector.setDisplayedValues(coinsAvailable);
                     selector.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+
+                    //Add selected coin to userCoins on clicking select
+                    btnSelect.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            userCoins.add(coinsAvailable[selector.getValue()]);
+                            renderCoinListView();
+                            dialog.dismiss();
+                        }
+                    });
+
+                    //Dismiss dialog if on clicking cancel
+                    btnCancel.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialog.dismiss();
+                        }
+                    });
 
                     dialog.create();
                     dialog.show();
@@ -93,26 +113,66 @@ public class WatchingActivity extends AppCompatActivity {
             }
         });
 
-        //This is a demo of generating a list of coin information
-        //This will be replaced with saved states of user-added favorites
-        //List items will be more complex than just text in the future
-        CoinAdapter watchingAdapter = new CoinAdapter(this, demoCoins);
+        final Handler handler = new Handler();
+        final int delay = 10000; //milliseconds
 
-        //Find list from activity_watching and set the adapter
-        LinearLayout mainContainer = (LinearLayout) findViewById(R.id.watching_ll_mainContainer);
-        ListView coinListView = (ListView) mainContainer.findViewById(R.id.watching_lv_coinList);
-        coinListView.setAdapter(watchingAdapter);
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                renderCoinListView();
+                toast(mContext, "coin list updated");
+                handler.postDelayed(this, delay);
+            }
+        }, delay);
+
+
 
         Log.i(STATE_TAG, "onCreate");
+    }
+
+    public void renderCoinListView(){
+        //Update puser coins based on db
+        SQLiteDatabase db = watchingCoinsDb.getReadableDatabase();
+        String[] cols = {watchingCoinsDb.RAW_FROMSYMBOL};
+        Cursor c = db.query(
+                watchingCoinsDb.TABLE_NAME,
+                cols,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+        while(c.moveToNext() && c != null) {
+            String currentCoin = c.getString(c.getColumnIndexOrThrow(watchingCoinsDb.RAW_FROMSYMBOL));
+
+            //Prevent coins from being added to the list again
+            if (!userCoins.contains(currentCoin)){
+                userCoins.add(currentCoin);
+            }
+            Log.i(TAG, "userCoin added: " + c.getString(c.getColumnIndexOrThrow(watchingCoinsDb.RAW_FROMSYMBOL)));
+        }
+
+        if(userCoins.size() > 0) {
+            //Update current information in db
+            PriceMultiFull priceMultiFull = new PriceMultiFull(mContext, userCoins);
+            priceMultiFull.sendRequest();
+
+            CoinAdapter watchingAdapter = new CoinAdapter(this, userCoins);
+
+            //Find list from activity_watching and set the adapter
+            LinearLayout mainContainer = (LinearLayout) findViewById(R.id.watching_ll_mainContainer);
+            ListView coinListView = (ListView) mainContainer.findViewById(R.id.watching_lv_coinList);
+            coinListView.setAdapter(watchingAdapter);
+        }
     }
 
     //Custom adapter to turn demoStringValues into a dynamic ListView
     private class CoinAdapter extends ArrayAdapter<String> {
         private final Context context;
-        private final String[] coins;
+        private final ArrayList<String> coins;
 
         //TODO: coins string array will be come far more complex will addition of API
-        public CoinAdapter(Context context, String[] coins) {
+        public CoinAdapter(Context context, ArrayList<String> coins) {
             super(context, R.layout.coin_view, coins);
             this.context = context;
             this.coins = coins;
@@ -142,16 +202,16 @@ public class WatchingActivity extends AppCompatActivity {
             TextView walletValue = (TextView) walletLayout.findViewById(R.id.watching_txt_walletValue);
             LinearLayout profitLayout = (LinearLayout) row2.findViewById(R.id.watching_ll_profitLayout);
             TextView profitValue = (TextView) profitLayout.findViewById(R.id.watching_txt_profitValue);
-            LinearLayout askLayout = (LinearLayout) row3.findViewById(R.id.watching_ll_askLayout);
-            TextView askValue = (TextView) askLayout.findViewById(R.id.watching_txt_askValue);
-            LinearLayout bidLayout = (LinearLayout) row3.findViewById(R.id.watching_ll_bidLayout);
-            TextView bidValue = (TextView) bidLayout.findViewById(R.id.watching_txt_bidValue);
+            LinearLayout priceLayout = (LinearLayout) row3.findViewById(R.id.watching_ll_priceLayout);
+            TextView priceValue = (TextView) priceLayout.findViewById(R.id.watching_txt_priceValue);
+            LinearLayout lastMarketLayout = (LinearLayout) row3.findViewById(R.id.watching_ll_lastMarketLayout);
+            TextView lastMarketValue = (TextView) lastMarketLayout.findViewById(R.id.watching_txt_lastMarketValue);
 
             //Set onClickListener to open a generic alert window
             editProperties.setOnClickListener(new View.OnClickListener(){
                 public void onClick(View v){
                     //Log the button that was clicked
-                    Log.i(BUTTON_CLICK, (coins[position] + " edit properties"));
+                    Log.i(BUTTON_CLICK, (coins.get(position) + " edit properties"));
 
                     //TODO: Remove the alert dialog, and replace with properties fragment or activity
                     //Create the alert dialog builder
@@ -159,38 +219,50 @@ public class WatchingActivity extends AppCompatActivity {
                     //It currently serves as a POC of OnClickListener
                     final AlertDialog.Builder futureDevelopment = new AlertDialog.Builder(context);
                     futureDevelopment.setTitle("AlertDialog");
-                    futureDevelopment.setMessage("Future development space for " + coins[position]);
+                    futureDevelopment.setMessage("Future development space for " + coins.get(position));
                     futureDevelopment.setCancelable(true);
 
                     AlertDialog alert = futureDevelopment.create();
                     alert.show();
-
-
                 }
             });
 
             //Change text to that of the current coin
-            nameValue.setText(coins[position]);
+            nameValue.setText(coins.get(position));
 
             //load states of coins from SQL database
             SQLiteDatabase db = watchingCoinsDb.getReadableDatabase();
-            String[] cols = {watchingCoinsDb.DISPLAY_CHANGEPCT24HOUR, watchingCoinsDb.DISPLAY_PRICE};
+            String[] cols = {watchingCoinsDb.DISPLAY_CHANGEPCT24HOUR,
+                    watchingCoinsDb.DISPLAY_PRICE,
+                    watchingCoinsDb.RAW_LASTMARKET};
             Cursor c = db.query(
                     watchingCoinsDb.TABLE_NAME,
                     cols,
                     "RAW_FROMSYMBOL=?",
-                    new String[]{coins[position]},
+                    new String[]{coins.get(position)},
                     null,
                     null,
                     null
             );
+            if(c != null && c.moveToFirst()) {
+                c.moveToFirst();
+                double display_changePct24Hour = c.getDouble(c.getColumnIndexOrThrow(watchingCoinsDb.DISPLAY_CHANGEPCT24HOUR));
+                if(display_changePct24Hour >= 0){
+                    changeValue.setTextColor(getResources().getColor(R.color.positive));
+                    downArrow.setVisibility(View.INVISIBLE);
+                    upArrow.setVisibility(View.VISIBLE);
+                } else{
+                    changeValue.setTextColor(getResources().getColor(R.color.negative));
+                    upArrow.setVisibility(View.INVISIBLE);
+                    downArrow.setVisibility(View.VISIBLE);
+                }
 
-            c.moveToFirst();
-            changeValue.setText(c.getString(c.getColumnIndexOrThrow(watchingCoinsDb.DISPLAY_CHANGEPCT24HOUR)));
-            askValue.setText(c.getString(c.getColumnIndexOrThrow(watchingCoinsDb.DISPLAY_PRICE)));
-            c.close();
-
-            Log.i(COIN_VIEW, "Coin view created for " + coins[position]);
+                changeValue.setText(display_changePct24Hour + "%");
+                priceValue.setText(c.getString(c.getColumnIndexOrThrow(watchingCoinsDb.DISPLAY_PRICE)));
+                lastMarketValue.setText(c.getString(c.getColumnIndexOrThrow(watchingCoinsDb.RAW_LASTMARKET)));
+                c.close();
+            }
+            Log.i(COIN_VIEW, "Coin view created for " + coins.get(position));
 
             return rowView;
         }
@@ -274,7 +346,11 @@ public class WatchingActivity extends AppCompatActivity {
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
+        renderCoinListView();
         Log.i(STATE_TAG, "onRestoreInstanceState");
     }
 
+    public void toast(Context context, String toastDisplay){
+        Toast.makeText(context, toastDisplay, Toast.LENGTH_SHORT).show();
+    }
 }

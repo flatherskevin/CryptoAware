26e55755f060a3d76fcc2fb58703191b03e6794c
flatherskevin <flatherskevin@gmail.com>
2017-09-30 16:05:39 -0400

    Settings activity added with preliminary options - no functionality yet

---
+++ app/src/main/java/com/flathers/cryptoaware/SettingsActivity.java
@@ -0,0 +1,78 @@
+package com.flathers.cryptoaware;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.CheckBox;
+import android.widget.Toast;
+import android.content.SharedPreferences;
+
+public class SettingsActivity extends AppCompatActivity {
+    private static final String TAG = WatchingActivity.class.getName();
+    final Context mContext = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        CheckBox notifcationsCheckBox = (CheckBox) findViewById(R.id.settings_chkbx_notifications);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(TAG, "onRestoreInstanceState");
+    }
+
+    public void toast(Context context, String toastDisplay){
+        Toast.makeText(context, toastDisplay, Toast.LENGTH_SHORT).show();
+    }
+}

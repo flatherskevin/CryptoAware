e2ce16e31955c9608943c5b78f21c660ff712aec
flatherskevin <flatherskevin@gmail.com>
2017-10-11 23:54:12 -0400

    Transaction table finalized

--- app/src/main/java/com/flathers/cryptoaware/TransactionsDb.java
+++ app/src/main/java/com/flathers/cryptoaware/TransactionsDb.java
@@ -3,6 +3,7 @@ package com.flathers.cryptoaware;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.icu.text.IDNA;
 import android.util.Log;
 
 /**
@@ -14,18 +15,30 @@ public class TransactionsDb extends SQLiteOpenHelper {
     public static final String DB_NAME = "CryptoAware.db";
     public static final String TABLE_NAME = "transactions";
     public static final int DB_VERSION = 1;
+    public static final String ID = "ID";
     public static final String RAW_FROMSYMBOL = WatchingCoinsDb.RAW_FROMSYMBOL;
-    public static final String TRANSACTION_PRICE = "TRANSACTION_PRICE";
-    public static final String MARKET_PRICE = "MARKET_PRICE";
-    public static final String MARKET = "MARKET";
+    public static final String TRANSACTION_QTY = "TRANSACTION_QTY";
+    public static final String MARKET_PRICE = "MARKET_PRICE_BTC";
     public static final String TRANSACTION_DATE = "TRANSACTION_DATE";
+    public static final String TOTAL_BTC = "TOTAL_BTC";
+    public static final String BUY_SELL = "BUY_SELL";
+    public static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS "
+            + TABLE_NAME                +   " ("
+            + ID                        +   " INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + RAW_FROMSYMBOL            +   " TEXT,"
+            + BUY_SELL                  +   " TEXT,"
+            + TRANSACTION_QTY           +   " REAL,"
+            + TRANSACTION_DATE          +   " TEXT,"
+            + MARKET_PRICE              +   " REAL,"
+            + TOTAL_BTC                 +   " REAL,"
+            + "FOREIGN KEY (" + RAW_FROMSYMBOL +") REFERENCES " + WatchingCoinsDb.TABLE_NAME + "(" + WatchingCoinsDb.RAW_FROMSYMBOL + "));";
 
     TransactionsDb(Context context){
         super(context, DB_NAME, null, DB_VERSION);
     }
 
     public void onCreate(SQLiteDatabase db){
-        //db.execSQL(CREATE_TABLE);
+        db.execSQL(CREATE_TABLE);
         Log.i(TAG, "Table created");
     }
 
